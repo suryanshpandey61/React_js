c2a0a9fd7f4c672920df1a8ca9eb2e327b7e91f8
suryanshpandey61 <suryansh.knipss061@gmail.com>
2024-11-27 20:21:38 +0530

    src file added

---
+++ Basics_2/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+const App = () => {
+    return (
+        <div>
+            <Navbar/>
+            <h1>This is App Component</h1>
+        </div>
+    )
+}
+
+export default App;
--- Basics_2/src/index.js
+++ Basics_2/src/index.js
@@ -1,4 +1,6 @@
 import { createRoot } from "react-dom/client";
+import App from "./App";
 let div = document.getElementById("root");
 
-createRoot(div).render(<h1>Hello React </h1>)
+//createRoot(div).render(<h1>Hello World</h1>);
+createRoot(div).render(<App></App>);
