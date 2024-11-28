824e3c779253eb4d19fda578060f2b3d86cee748
suryanshpandey61 <suryansh.knipss061@gmail.com>
2024-11-28 21:24:55 +0530

    FBC states added

--- Basics_2/src/App.jsx
+++ Basics_2/src/App.jsx
@@ -1,8 +1,12 @@
 import Navbar from "./components/Navbar";
+import NavbarCBC from "./components/NavbarCBC";
+import State from "./states/fbc/State";
 const App = () => {
     return (
         <div>
-            <Navbar/>
+            {/* <Navbar/>
+            <NavbarCBC/> */}
+            <State/>
             <h1>This is App Component</h1>
         </div>
     )
