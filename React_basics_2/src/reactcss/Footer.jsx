97294e7bfd31f941bec3417662ff7b6e9f7d0ce9
suryanshpandey61 <suryansh.knipss061@gmail.com>
2024-12-06 13:49:55 +0530

    moduleCssAdded

--- React_basics_2/src/reactcss/Footer.jsx
+++ React_basics_2/src/reactcss/Footer.jsx
@@ -2,7 +2,7 @@ import style from "./footer.module.css";
 const Footer = () => {
     return(
         <footer id={style.footer}>
-            <h1 id={style.heading1}>Footer Component</h1>
+            <h1 id={style.heading1}>This is Footer  Component</h1>
         </footer>
     )
 }
